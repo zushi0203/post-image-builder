07807912370f45d2c114f619d47e438f46aa33b3
zushi <ringochelsea.jp@gmail.com>
2025-08-25 20:48:53 +0900

    fix: ensure consistent quotation marks in Vite configuration and add newline at end of GIF encoder type definition

--- packages/application/vite.config.ts
+++ packages/application/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/post-image-builder/',
+  base: "/post-image-builder/",
   resolve: {
     alias: {
       "@": "/src",
